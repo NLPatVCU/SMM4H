8685de6e8c6f37955d94d7414ab812e9ed3771dd
Cora <38634654+afortiori@users.noreply.github.com>
2020-06-16 14:24:44 -0400

    Delete convert_word2vec.py

--- convert_word2vec.py
+++
@@ -1,4 +0,0 @@
-from gensim.models.keyedvectors import KeyedVectors
-
-model = KeyedVectors.load_word2vec_format('../embeddings/word2vectwitter.bin', binary=True)
-model.save_word2vec_format('../embeddings/word2vectwitter.txt', binary=False)
